877eec710771b1f21f08d1b040cb6e7d61621466
Sidharth-A-691 <sidharth69122@gmail.com>
2024-10-29 17:43:55 +0530

    first commit - routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { ErrorComponent } from './error/error.component';
+import { RegisterComponent } from './register/register.component';
+import { HeaderComponent } from './header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: "home", component: HomeComponent},
+    {path: "services",component: ServicesComponent},
+    {path: "contactus", component: ContactusComponent},
+    {path: "register", component: RegisterComponent},
+    {path: "**", component: ErrorComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { ErrorComponent } from './error/error.component';
+import { HeaderComponent } from './header/header.component';
+import { RegisterComponent } from './register/register.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicesComponent,
+    ContactusComponent,
+    ErrorComponent,
+    HeaderComponent,
+    RegisterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
